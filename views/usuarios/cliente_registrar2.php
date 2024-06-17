fbac551121e202ac6962b1c27db42deef09b8f22
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-17 17:30:02 -0500

    Update cliente_registrar2.php

--- views/usuarios/cliente_registrar2.php
+++ views/usuarios/cliente_registrar2.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['Correo'])) {
 }
 ?>
 <?php require '../../includes/_db.php'; ?>
-<?php require '../../includes/_header.php'; ?>
+<?php require '../../includes/_header2.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
