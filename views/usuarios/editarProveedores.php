354a5c2cdbb8cb81e8e5e0412c96a05e386a7f77
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 19:59:35 -0500

    Update editarProveedores.php
    Cambio de todo el code prueva 1

--- views/usuarios/editarProveedores.php
+++ views/usuarios/editarProveedores.php
@@ -1,20 +1,19 @@
 <?php
-
 require '../../includes/conexionBD.php';
 
-$id = $conn->real_escape_string($_POST['IdProveedor']);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $IdProveedor = $_POST['IdProveedor'];
+    $sql = "SELECT * FROM tblproveedores WHERE IdProveedor = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $IdProveedor);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-
-$sqlEditar = "SELECT IdProveedor, Nit, NombreEmpresa, NombreProveedor, ProductoVender, Direccion, Telefono, Correo FROM tblproveedores WHERE IdProveedor=$id LIMIT 1";
-$resultado = $conn->query($sqlEditar);
-$rows = $resultado->num_rows;
-
-$proveedores = [];
-
-if($rows > 0){
-    $proveedores = $resultado->fetch_array();
+    if ($result->num_rows > 0) {
+        $data = $result->fetch_assoc();
+        echo json_encode($data);
+    } else {
+        echo json_encode([]);
+    }
 }
-
-echo json_encode($proveedores, JSON_UNESCAPED_UNICODE);
-
 ?>
