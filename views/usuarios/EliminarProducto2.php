e836f50ae36cecbec3b99a6753a15dcf6a0a42f1
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:17:28 -0500

    Update EliminarProducto2.php

--- views/usuarios/EliminarProducto2.php
+++ views/usuarios/EliminarProducto2.php
@@ -7,6 +7,15 @@ $IdProducto = $conn->real_escape_string($_POST['IdProducto']);
 $sql = "DELETE FROM tblproductos WHERE IdProducto = $IdProducto";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblproductos SET IdProducto = @count:= @count + 1;
+        ALTER TABLE tblproductos AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: producto_registrar2.php');
