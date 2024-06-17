c11a8fc698103415aad46e13522accfbf309882d
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:22:18 -0500

    Update EliminarHistoria.php

--- views/usuarios/EliminarHistoria.php
+++ views/usuarios/EliminarHistoria.php
@@ -7,6 +7,15 @@ $IdHistoriaClinica = $conn->real_escape_string($_POST['IdHistoriaClinica']);
 $sql = "DELETE FROM tblhistoriasclinicass WHERE IdHistoriaClinica = $IdHistoriaClinica";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblhistoriasclinicass SET IdHistoriaClinica = @count:= @count + 1;
+        ALTER TABLE tblhistoriasclinicass AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: historias_registrar.php');
