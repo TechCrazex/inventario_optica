c60c6bfda49a5a48cc2bacbe8751027b312f6c61
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:17:56 -0500

    Update EliminarProveedor.php

--- views/usuarios/EliminarProveedor.php
+++ views/usuarios/EliminarProveedor.php
@@ -7,6 +7,15 @@ $IdProveedor = $conn->real_escape_string($_POST['IdProveedor']);
 $sql = "DELETE FROM tblproveedores WHERE IdProveedor = $IdProveedor";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblproveedores SET IdProveedor = @count:= @count + 1;
+        ALTER TABLE tblproveedores AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: registrarProveedor.php');
