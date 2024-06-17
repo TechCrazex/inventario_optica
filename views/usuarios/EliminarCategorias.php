5025e1f364c4fc0a3f52e8b4e12e70c030387cbc
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:19:04 -0500

    Update EliminarCategorias.php

--- views/usuarios/EliminarCategorias.php
+++ views/usuarios/EliminarCategorias.php
@@ -7,6 +7,15 @@ $IdCategoria = $conn->real_escape_string($_POST['IdCategoria']);
 $sql = "DELETE FROM tblcategoriasproductos WHERE IdCategoria = $IdCategoria";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblcategoriasproductos SET IdCategoria = @count:= @count + 1;
+        ALTER TABLE tblcategoriasproductos AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: categorias_registrar.php');
