93a5ff2589d1ff4fe3e503f74f22e3663555121d
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-17 17:39:36 -0500

    Update historias_registrar2.php

--- views/usuarios/historias_registrar2.php
+++ views/usuarios/historias_registrar2.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['Correo'])) {
 }
 ?>
 <?php require '../../includes/_db.php'; ?>
-<?php require '../../includes/_header.php'; ?>
+<?php require '../../includes/_header2.php'; ?>
 <?php 
 date_default_timezone_set('America/Bogota');
 $fechaActual = date('Y-m-d');
