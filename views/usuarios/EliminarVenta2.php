0e259d7d003dac5f5c13ae1d99a1e2fd52cdb156
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:23:33 -0500

    Update EliminarVenta2.php

--- views/usuarios/EliminarVenta2.php
+++ views/usuarios/EliminarVenta2.php
@@ -7,6 +7,15 @@ $IdVenta = $conn->real_escape_string($_POST['IdVenta']);
 $sql = "DELETE FROM tblventas WHERE IdVenta = $IdVenta";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblventas SET IdVenta = @count:= @count + 1;
+        ALTER TABLE tblventas AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: venta_registrar2.php');
