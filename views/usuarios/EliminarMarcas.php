41e5266dea48c92214755fdeed6904501838adfe
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:22:47 -0500

    Update EliminarMarcas.php

--- views/usuarios/EliminarMarcas.php
+++ views/usuarios/EliminarMarcas.php
@@ -7,6 +7,15 @@ $IdMarca = $conn->real_escape_string($_POST['IdMarca']);
 $sql = "DELETE FROM tblmarcasproductos WHERE IdMarca = $IdMarca";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblmarcasproductos SET IdMarca = @count:= @count + 1;
+        ALTER TABLE tblmarcasproductos AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: marcas_registrar.php');
