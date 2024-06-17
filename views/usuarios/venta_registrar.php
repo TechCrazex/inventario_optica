28766e3108369b630dee81c5b3cce972b2f6421f
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-17 17:28:24 -0500

    Update venta_registrar.php

--- views/usuarios/venta_registrar.php
+++ views/usuarios/venta_registrar.php
@@ -9,6 +9,10 @@ if (!isset($_SESSION['Correo'])) {
 <html lang="en">
 <?php require '../../includes/_db.php'; ?>
 <?php require '../../includes/_header.php'; ?>
+<?php 
+date_default_timezone_set('America/Bogota');
+$fechaActual = date('Y-m-d');
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <link rel="stylesheet" href="../../css/Style.css"> 
 <!-- Bootstrao iconos PNG -->
