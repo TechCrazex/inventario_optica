74367630452e594dc1eb4be4d1362c6c55214de7
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:28:05 -0500

    Update registrarCompras2.php

--- views/usuarios/registrarCompras2.php
+++ views/usuarios/registrarCompras2.php
@@ -3,7 +3,6 @@
 require '../../includes/conexionBD.php';
 
 // Obtener los datos del formulario
-$NumeroCompra = $conn->real_escape_string($_POST['NumeroCompra']);
 $IdProveedor = $conn->real_escape_string($_POST['proveedor']);
 $IdProducto = $conn->real_escape_string($_POST['producto']);
 $IdEmpleado = $conn->real_escape_string($_POST['Empleado']);
@@ -12,8 +11,6 @@ $PrecioUnidad = $conn->real_escape_string($_POST['PrecioUnidad']);
 $PrecioTotal = $conn->real_escape_string($_POST['PrecioTotal']);
 $FechaCompra = $conn->real_escape_string($_POST['FechaCompra']);
 $Observacion = $conn->real_escape_string($_POST['Observacion']);
-
-// Suponiendo que ya tienes las variables $CantidadComprada y $IdProducto
 
 // Consulta para obtener la cantidad actual en stock del producto
 $sqlStock = "SELECT CantidadStock FROM tblproductos WHERE IdProducto = '$IdProducto'";
@@ -31,15 +28,15 @@ if ($resultStock->num_rows > 0) {
     if ($conn->query($sqlUpdateStock)) {
         // Éxito al actualizar la cantidad en stock
     } else {
-        // Error al actualizar la cantidad en stock
+        echo 'Error al actualizar la cantidad en stock: ' . $conn->error;
     }
 } else {
-    // No se encontró el producto
+    echo 'No se encontró el producto';
 }
 
 // Insertar los datos en la tabla de compras
-$sql = "INSERT INTO tblcompras (NumeroCompra, IdProveedor, IdProducto, IdEmpleado, CantidadComprada, PrecioUnidad, PrecioTotal, FechaCompra, Observación) 
-    VALUES ('$NumeroCompra', '$IdProveedor', '$IdProducto', '$IdEmpleado', '$CantidadComprada', '$PrecioUnidad', '$PrecioTotal', '$FechaCompra', '$Observacion')";
+$sql = "INSERT INTO tblcompras (IdProveedor, IdProducto, IdEmpleado, CantidadComprada, PrecioUnidad, PrecioTotal, FechaCompra, Observación) 
+    VALUES ('$IdProveedor', '$IdProducto', '$IdEmpleado', '$CantidadComprada', '$PrecioUnidad', '$PrecioTotal', '$FechaCompra', '$Observacion')";
 
 if ($conn->query($sql)) {
     $IdCompra = $conn->insert_id; // Asigna el insert_id después de la inserción
