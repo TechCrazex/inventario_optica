9497b10ae82379738035cf78e4e081ac24baa32e
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 20:18:57 -0500

    Update registrarProveedor.php
    Code original

--- views/usuarios/registrarProveedor.php
+++ views/usuarios/registrarProveedor.php
@@ -134,4 +134,4 @@ if (!isset($_SESSION['Correo'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="buscador/buscador.js"></script>
 </body>
-</html>   
+</html>
