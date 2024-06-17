d82dd32cec49223e14df41399bb2d1707c5d51af
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-17 16:03:30 -0500

    Update compras_registrar.php

--- views/usuarios/compras_registrar.php
+++ views/usuarios/compras_registrar.php
@@ -34,7 +34,7 @@ $proveedor = $conn->query($queryProveedor);
 $queryProducto = "SELECT * FROM tblproductos";
 $producto = $conn->query($queryProducto);
 
-$queryEmpleado = "SELECT * FROM tblempleados WHERE IdRol = 1";
+$queryEmpleado = "SELECT * FROM tblempleados WHERE IdRol = 2";
 $empleado = $conn->query($queryEmpleado);
 ?>
 
