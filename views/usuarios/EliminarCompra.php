023de7e0cc99d9e0483a728d8b2bec6957862ca9
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:21:47 -0500

    Update EliminarCompra.php

--- views/usuarios/EliminarCompra.php
+++ views/usuarios/EliminarCompra.php
@@ -7,6 +7,15 @@ $IdCompra = $conn->real_escape_string($_POST['IdCompra']);
 $sql = "DELETE FROM tblcompras WHERE IdCompra = $IdCompra";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblcompras SET IdCompra = @count:= @count + 1;
+        ALTER TABLE tblcompras AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: compras_registrar.php');
