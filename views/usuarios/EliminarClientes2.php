b6d1874ed372b54ec2ee95419822fde80314b45f
TechCrazex <134068460+TechCrazex@users.noreply.github.com>
2024-06-16 21:19:47 -0500

    Update EliminarClientes2.php

--- views/usuarios/EliminarClientes2.php
+++ views/usuarios/EliminarClientes2.php
@@ -7,6 +7,15 @@ $IdCliente = $conn->real_escape_string($_POST['IdCliente']);
 $sql = "DELETE FROM tblclientes WHERE IdCliente = $IdCliente";
 
 if ($conn->query($sql)) {
+    // Reiniciar IDs
+    $sqlReindex = "
+        SET @count = 0;
+        UPDATE tblclientes SET IdCliente = @count:= @count + 1;
+        ALTER TABLE tblclientes AUTO_INCREMENT = 1;
+    ";
+    $conn->multi_query($sqlReindex);
+    // Asegurarse de que todas las consultas se hayan ejecutado
+    while ($conn->next_result()) {;}
 }
 
 header('Location: cliente_registrar2.php');
